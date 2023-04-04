e59751cbb3c496736a52b1b7040046809fa97479
Oliveiralucass <gf.oliveiralucas@gmail.com>
2023-04-04 09:00:12 -0300

    Responsiveness

--- components/RightContent.js
+++ components/RightContent.js
@@ -9,13 +9,13 @@ export default function RightContent() {
             <Image src={'/images/water-jar.jpg'} width={160} height={160} alt='Water Jar'/>
 
             <div className={styles.rightContentInfo}>
-                <h1>Best drink</h1>
+                <h2>Best drink</h2>
                 <button>FOR FREE</button>
                 <p>Long term health</p>
             </div>
         </div>
         
-        <p>click here and join us with these magical benefits</p>
+        <p className={styles.rightContentClickHere}>click here and join us with these magical benefits</p>
     </div>
   )
 }
